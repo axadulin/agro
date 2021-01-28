91751abfce3d8b6057979fb994e77e4af2812e00
axadulin <37653661+axadulin@users.noreply.github.com>
2021-01-28 12:48:25 +0500

    Delete agrofilesdart directory

--- agrofilesdart/lib/models/album_model.dart
+++
@@ -1,7 +0,0 @@
-class AlbumModel{
-  String title;
-  String image;
-  String info;
-
-  AlbumModel(this.title, this.image, this.info);
-}
--- agrofilesdart/lib/models/resident_model.dart
+++
@@ -1,8 +0,0 @@
-class ResidentModel{
-  String name;
-  String direction;
-  String telNum;
-  String image;
-
-  ResidentModel({this.name, this.direction, this.telNum, this.image});
-}
